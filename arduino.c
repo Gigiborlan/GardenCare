2f318f54bf8d99fdae41045b88f331ccd6249776
Giovana Orlandini <31085094+Gigiborlan@users.noreply.github.com>
2024-10-08 22:22:55 -0300

    Add function to turn off pump

--- arduino.c
+++ arduino.c
@@ -4,6 +4,8 @@ const char turnLampOff = '3';
 const char turnPumpOn = '4';
 
 const int moistureSensorPin = A0;
+const int lampPin = 12;
+const int pumpPin = 13;
 
 unsigned long pumpTurnedOnBeginTime = 0;
 unsigned long currentTime = 0;
@@ -12,8 +14,21 @@ const unsigned long pumpTotalTimeOn = 3000;
 int moistureSensorValue = 0;
 char rasberryComand = 0;
 
+bool shouldTurnPumpOff(){
+  if ( pumpTurnedOnBeginTime != 0 ){
+    currentTime = millis();
+    if ( currentTime - pumpTurnedOnBeginTime >= pumpTotalTimeOn ){
+      pumpTurnedOnBeginTime = 0;
+      return true;
+    }
+  }
+  return false;
+}
+
 void setup() {
   Serial.begin(9600);
+  pinMode(lampPin, OUTPUT);
+  pinMode(pumpPin, OUTPUT);
 }
 
 void loop() { 
@@ -24,28 +39,23 @@ void loop() {
       Serial.println(moistureSensorValue);
     }
     if (rasberryComand == turnLampOn ) {
-      //digital write
+      digitalWrite(lampPin, HIGH);
       Serial.println("Lamp turned on");
     }
     if (rasberryComand == turnLampOff ) {
-      //digital write
+      digitalWrite(lampPin, LOW);
       Serial.println("Lamp turned off");
     }
     if (rasberryComand == turnPumpOn ) {
-      //digital write
+      digitalWrite(pumpPin, HIGH);
       pumpTurnedOnBeginTime = millis();
       Serial.println("Pump turned on");
     }
   }
   
-  
-  if ( pumpTurnedOnBeginTime != 0 ){
-    currentTime = millis();
-    if ( currentTime - pumpTurnedOnBeginTime >= pumpTotalTimeOn ){
-      //digital write
-      pumpTurnedOnBeginTime = 0;
-      Serial.println("Pump turned off");
-    }
+  if ( shouldTurnPumpOff() == true ){
+   digitalWrite(pumpPin, LOW);
+   Serial.println("Pump turned off");
   }
 
 }
